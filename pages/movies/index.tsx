ab7d38c495994dacdbec6970e7616f2d588c661e
Andrés Pardo Rodríguez <andres@Andress-MacBook-Pro.local>
2022-12-08 23:14:15 +0100

    added endpoint to discover genre (sci-fi) with space keyword

--- pages/movies/index.tsx
+++ pages/movies/index.tsx
@@ -7,27 +7,43 @@ const myLoader = ({ src }) => {
     return `${process.env.URL_IMAGE }${src}`
   }
 
+
+ const defaultEndPoint = `${process.env.API_URL}discover/movie?api_key=${process.env.API_KEY}&with_genres=878&year=1960&with_keywords=space`;
+ console.log(defaultEndPoint)
+
+
+const handleSubmit = (e) =>{
+    e.preventDefault();
+};
+
+const handleChange = (e) => {
+
+};
+
 const MovieList = ({movies}) => {
     console.log(movies.results, 'here are movies')
+  
     return(
         <div>
-             <form className={styles.form}>
-                 <input type='text' placeholder='search movie' className='search'></input>
+             <form className={styles.form} onSubmit={handleSubmit}>
+                 <input type='text' placeholder='search movie' className='search' onChange={handleChange}/>
                  <button>search</button>
              </form>
 
         <ul className={styles.grid}>
-            {movies.results.map( (el, index) => {   
+            {movies.results?.map( (el, index) => {   
+                const { title, backdrop_path } = el;
                 return (
                 <li key={index} className={styles.card}>
-                <h1>{el.title}</h1>
+                <h1>{title}</h1>
+                { backdrop_path ?
                 <Image 
                     className={styles.images} 
                     width={500} 
                     height={350} 
                     loader={myLoader} 
-                    src={el.backdrop_path} 
-                    alt="Picture of the author"/>
+                    src= {backdrop_path}
+                    alt="Picture of the author"/> : ''}
                 </li>
             )}
             )}
@@ -39,7 +55,7 @@ const MovieList = ({movies}) => {
 };
 
 export const getStaticProps = async () => {
-    const data = await Axios.get(`${process.env.API_URL}movie/popular?api_key=${process.env.API_KEY}&language=en-US&page=1`);
+    const data = await Axios.get(defaultEndPoint);
     return{
         props:{
             movies: data.data,
